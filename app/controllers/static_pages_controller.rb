2e69710e0e50f6e217c2dc042d5096bf7a618046
Nicolai Seerup <mail@soundtracktor.com>
2015-12-18 03:57:35 +0100

    daily commic parser

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,15 @@
+require 'nokogiri'
+require 'open-uri'
+class StaticPagesController < ApplicationController
+  def frontpage
+    url = "http://news.yahoo.com/comics"
+    doc = Nokogiri::HTML(open(url))
+    array = []
+
+    doc.css("li.comic-item").each do |item|
+      array << item.at_css(".comic-image").children.at_css('img').attr('src')
+    end
+
+    @image_url = array.sample
+  end
+end
